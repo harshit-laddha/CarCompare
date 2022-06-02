237079c039e1d199ee844cdfe7227914e09e919b
Harshit Laddha <harshit.laddha@paytm.com>
2022-06-02 09:50:59 +0530

    Support for comfortFeature

--- src/main/java/com/example/carcompare/model/ModelVariant.java
+++ src/main/java/com/example/carcompare/model/ModelVariant.java
@@ -2,6 +2,7 @@ package com.example.carcompare.model;
 
 import com.example.carcompare.constants.Constants;
 import com.example.carcompare.enums.Category;
+import com.example.carcompare.enums.features.ComfortFeature;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,6 +11,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.Map;
 import java.util.Objects;
 
 @Document(collection = "variant")
@@ -27,6 +29,8 @@ public class ModelVariant extends Model implements Serializable {
 
     private Engine engine;
     private Dimension dimension;
+
+    private Map<ComfortFeature, Object> comfortFeatures;
 
     @Override
     public String getSequenceName() {
@@ -53,11 +57,11 @@ public class ModelVariant extends Model implements Serializable {
         ModelVariant that = (ModelVariant) o;
         return Float.compare(that.price, price) == 0 && Objects.equals(variantName, that.variantName)
                 && Objects.equals(image, that.image) && Objects.equals(engine, that.engine)
-                && Objects.equals(dimension, that.dimension);
+                && Objects.equals(dimension, that.dimension) && comfortFeatures.equals(that.getComfortFeatures());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), variantName, price, image, engine, dimension);
+        return Objects.hash(super.hashCode(), variantName, price, image, engine, dimension, comfortFeatures);
     }
 }
